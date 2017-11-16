a5b34289aa9c6e5d1791b4c6301e41c0d9f91b0a
dansbands <dansbands@hotmail.com>
2017-11-16 11:31:33 -0500

    add more songs, view setlists

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -10,13 +10,13 @@ class CLI
   end
 
   def get_user_input
-    gets.chomp
+    input = gets.chomp
+    input == "exit" ? goodbye : input
   end
 
   def option
     prompt_user
     input = get_user_input
-
     if input == "1"
       puts "Enter a title for your new setlist"
       title = get_user_input
@@ -24,12 +24,31 @@ class CLI
       puts "Created new setlist '#{title}'"
       add_songs(setlist)
     elsif input == "2"
-      puts "Here are the existing setlists:"
-      setlists = Setlist.all.each do |list|
-        puts "#{list.id}. #{list.name}"
-      end
+      option_2
     else
       invalid_selection
+    end
+  end
+
+# can I just use add_songs here?
+  def option_2
+    puts "Here are the existing setlists:"
+    setlists = Setlist.all.each do |list|
+      puts "#{list.id}. #{list.name}"
+    end
+    choose_setlist
+  end
+
+  def choose_setlist
+    puts "Choose a setlist by number from the list above:"
+    setlist = get_user_input
+    choice = Setlist.all.select do |list|
+      list.id.to_s == setlist
+    end
+    puts "Setlist #'#{choice.first.id} - #{choice.first.name}'"
+    puts "Songs:"
+    choice.first.songs.each do |song|
+      puts "#{song.id}. #{song.title}"
     end
   end
 
@@ -38,16 +57,7 @@ class CLI
     option
   end
 
-  def yes_no(input, setlist)
-    if input.upcase == 'Y'
-      add_a_song(setlist)
-    elsif input.upcase == 'N'
-      goodbye
-    else
-      add_songs
-    end
-  end
-
+# can i reuse this method in option_2
   def add_songs(setlist)
     if setlist.list_songs == []
       puts "There are no songs in your setlist"
@@ -60,6 +70,16 @@ class CLI
     yes_no(input, setlist)
   end
 
+  def yes_no(input, setlist)
+    if input.upcase == 'Y'
+      add_a_song(setlist)
+    elsif input.upcase == 'N'
+      goodbye
+    else
+      add_songs(setlist)
+    end
+  end
+
   def add_a_song(setlist)
     puts "Enter the title of the song:"
     title = get_user_input
@@ -69,7 +89,7 @@ class CLI
     setlist_id = setlist.id
     order = setlist.songs.length + 1
     setlistsong = SetlistSong.create(order: order, song_id: song_id, setlist_id: setlist_id)
-    puts "Added #{song} to #{setlist.name} at position #{order}."
+    puts "Added '#{song.title}' to '#{setlist.name}' at position '#{order}'."
     puts "Here's your updated setlist:"
     new_list = Setlist.all.select do |item|
       item == setlist
@@ -79,6 +99,15 @@ class CLI
     new_list.songs.each do |song|
       puts "#{count}. #{song.title}"
       count += 1
+    end
+    puts "Choose an Option:
+          1. Add Another Song to This Setlist
+          2. View All Setlists"
+    input = get_user_input
+    if input == "1"
+      add_a_song(setlist)
+    elsif input == "2"
+      option_2
     end
   end
 
@@ -90,5 +119,6 @@ class CLI
   def goodbye
     puts "Thanks for using Setlist Organizer."
     puts "Goodbye!"
+    exit
   end
 end
